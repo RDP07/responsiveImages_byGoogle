4310250d4ab7d19ff91774948134e03572fb0db3
RDP <rdupaw@gmail.com>
2017-04-04 23:07:00 -0500

    Change pixel size back to 1200px in Gruntfile.js to make load data 1.5mb

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         options: {
           engine: 'im',
           sizes: [{
-            width: 1600,
+            width: 1200,
             suffix: '_large_2x',
             quality: 30
           }]
